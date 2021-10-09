32d0ac9fbea8e62aebdddfccda51be9443a61fef
tarek yahia <tarekyehya1999@gmail.com>
2021-10-09 21:08:40 +0000

    casting price column

--- models/staging/stg_raw_food_orders.sql
+++ models/staging/stg_raw_food_orders.sql
@@ -1,7 +1,7 @@
 select   order_id,/* dublicate order_id the all row not dublicated */
         customer_id,
         REGEXP_REPLACE(lower(item_name),'[^a-z]','') as item_name, /* lower case,delete numbers */
-        REGEXP_REPLACE(item_price,'[^0-9]','') as price, /* lower case,delete numbers */
+        cast(REGEXP_REPLACE(item_price,'[^0-9]','') as int64) as price, /* lower case,delete numbers */
         date
 from 
     {{source('gad_resturant','raw_food_orders')}}
